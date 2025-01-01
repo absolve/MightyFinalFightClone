7b315af150658a4a56e586208c4bc6d21906f297
absolve <wangding1992@126.com>
2025-01-01 22:27:55 +0800

    更新功能

---
+++ script/welcome.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+
+
+func _ready():
+	OS.center_window()
+	VisualServer.set_default_clear_color("#000")
+	
+
+
+func _input(event):
+	if Input.is_action_just_pressed("start"):
+		
+		pass
+	
+
