90add5613608012b6ea454c6e755c48989f756f7
absolve <wangding1992@126.com>
2025-04-22 18:42:09 +0800

    更新功能

---
+++ script/cody.gd
@@ -0,0 +1,10 @@
+extends "res://script/object.gd"
+
+
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+
--- script/guy.gd
+++ script/guy.gd
@@ -3,6 +3,8 @@ extends "res://script/object.gd"
 
 
 
+
+# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
