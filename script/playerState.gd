96f3c8aefb60059567dc5e52d11e325c89570c66
absolve <wangding1992@126.com>
2025-05-19 15:49:04 +0800

    更新功能

--- script/playerState.gd
+++ script/playerState.gd
@@ -23,6 +23,15 @@ func physics_update(_delta: float) -> void:
 	elif xDir<0:
 		player.vec.x=-player.speed
 	
+	var yDir=Input.get_axis("p1_up","p1_down")
+	if yDir==0:
+		player.vec.y=0
+	elif yDir>0:
+		player.vec.y=player.speed
+	elif yDir<0:
+		player.vec.y=-player.speed	
+	
+		
 	player.move_and_slide(player.vec)
 	
 func animation():
